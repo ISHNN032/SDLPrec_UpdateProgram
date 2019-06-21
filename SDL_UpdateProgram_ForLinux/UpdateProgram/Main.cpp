8bab2038b5b2666466478059555332820b5e144e
ISHNN <ishnn032n@gmail.com>
2019-06-21 11:56:27 +0900

    STAGE: Linux Project File

---
+++ SDL_UpdateProgram_ForLinux/UpdateProgram/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "MainActivity.h"
+
+int main(int argc, char* args[]) {
+	std::cout << "Main Activity Started!" << std::endl;
+
+	MainActivity mainActivity("Factory", 1024, 600);
+	mainActivity.~MainActivity();
+	/*
+	std::cout << "Type 'exit' to end program" << std::endl << ">> ";
+	while (true) {
+		std::string a = "";
+		std::cin >> a;
+		if (a == "exit") break;
+	}
+	*/
+
+	return EXIT_SUCCESS;
+}
