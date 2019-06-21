2b17cea8769d2dd945e95e7033af3f9fd7f51ceb
ISHNN <ishnn032n@gmail.com>
2019-06-21 11:30:55 +0900

    STAGE: Latest Project File

---
+++ SDL_UpdateProgram/SDL_UpdateProgram/Scripts/Global.cpp
@@ -0,0 +1,11 @@
+#include "Global.h"
+#include "LTexture.h"
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//Globally used font
+TTF_Font* gFont = NULL;
