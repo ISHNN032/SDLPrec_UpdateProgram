55e4101589a4a97e02517ff38b4e64116e759e50
ISHNN <ishnn032n@gmail.com>
2019-06-26 10:12:26 +0900

    ADD: UpdateManager

---
+++ SDL_UpdateProgram_ForLinux/UpdateProgram/UpdateManager.cpp
@@ -0,0 +1,156 @@
+#include "UpdateManager.h"
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+UpdateManager::UpdateManager(){
+	
+}
+
+UpdateManager::~UpdateManager(){
+
+}
+
+bool UpdateManager::updateFW(){
+	char cmd[100];
+	char ver_info_cur[50] = {0,};
+	char ver_info_update[50] = {0,};
+	int execute_update = 0;
+
+	struct dirent **items;
+	int nitems, i, j;
+	
+    if(exist_dir(update_path)) {
+		printf("Fail to create %s directory!!!\n", update_path);
+		return false;
+	}
+
+	memset(cmd, 0, 100);
+	sprintf(cmd, "cp %s %s", update_file_img, update_file);
+	system(cmd);
+
+	memset(cmd, 0, 100);
+	sprintf(cmd, "unzip %s -d %s", update_file, update_path);
+	system(cmd);
+
+	sleep(1);
+
+	if(exist_file(update_file)) {
+		return false;
+	}
+	
+	if ( check_ver_file("/root", ver_info_cur) ) { 			
+		if ( check_ver_file("/tmp/fw/root", ver_info_update) ) {
+			if( strncmp(ver_info_cur, ver_info_update, strlen(ver_info_update)) != 0 ) {
+				execute_update = 1;
+			}
+			else {
+				//no need to update
+			}
+		}
+	}
+	else
+		execute_update = 1;
+		
+	if(!execute_update) 
+		return 0;
+
+	nitems = scandir(update_path, &items, NULL, alphasort);
+
+	for(i=0;i<nitems;i++) {
+		if(!strcmp(items[i]->d_name, ".") || !strcmp(items[i]->d_name, ".."))
+			continue;
+
+		sleep(1);
+
+		if(!strcmp(items[i]->d_name, "uboot")) { 
+
+		}
+		else if(!strcmp(items[i]->d_name, "boot.img")) {
+			memset(cmd, 0, 100);
+			sprintf(cmd, "dd if=%s/%s of=/dev/mmcblk0p6 bs=2048", update_path, items[i]->d_name);
+			printf("execute update kernel cmd : %s\n", cmd);
+			system(cmd);
+		}
+		else {
+			/* Remove under "/root/" directory. */
+			memset(cmd, 0, 100);
+			sprintf(cmd, "rm -rf /root/*");
+			printf("execute update dir cmd : %s\n", cmd);
+			system(cmd);
+			usleep(1000);
+
+			/* Copying newer files. */
+			memset(cmd, 0, 100);
+			sprintf(cmd, "cp -a %s/%s/* /%s", update_path, items[i]->d_name, items[i]->d_name);
+			printf("execute update dir cmd : %s\n", cmd);
+			system(cmd);
+
+			memset(cmd, 0, 100);
+			sprintf(cmd, "chmod -R 755 /%s/*", items[i]->d_name);
+			system(cmd);
+		}		
+	}
+
+	memset(cmd, 0, 100);
+	sprintf(cmd, "rm -rf %s", update_file_img);
+	system(cmd);
+
+	//Update End
+
+
+	sleep(10);
+
+	system("reboot");
+
+    return true;
+}
+
+bool UpdateManager::exist_dir(char *dir)
+{
+	DIR *dp  = opendir(dir);
+
+	if(dp == NULL)
+	{
+		return -1;
+	}
+	else
+	{
+		closedir(dp);
+		return 0;
+	}
+
+}
+
+bool UpdateManager::exist_file(char* filename){
+	int fd = open(filename,O_RDONLY);
+	if(fd != -1) {
+		close(fd);
+		return 0;
+	}
+	else
+		return -1;
+}
+
+bool UpdateManager::check_ver_file(char* path, char* file_name)
+{	
+	bool ret = true;
+	struct dirent **items = NULL;
+	char *f_name;
+	int FileIdx = 0;
+	char tmp[50] = {0,};
+	int nitems = scandir(path, &items, NULL, alphasort);
+	while(FileIdx < nitems) {
+		sprintf(tmp, "%s", items[FileIdx]->d_name);
+		f_name = strtok(tmp, ".");
+		if((f_name != NULL) && (strcmp(f_name, "SYS") == 0)) {
+			sprintf(file_name, "%s", items[FileIdx]->d_name);
+			ret = false;
+			break;
+		}
+		FileIdx++;
+	}
+	return ret;	
+}
---
+++ SDL_UpdateProgram_ForLinux/UpdateProgram/UpdateManager.h
@@ -0,0 +1,43 @@
+#ifndef _UPDATEMANAGER_
+#define _UPDATEMANAGER_
+
+#define MOUNT_DEV_EXIST          (0)
+#define MOUNT_DEV_NOT_EXIST      (-1)
+
+#define MOUNT_POINT_EXIST        (0)
+#define MOUNT_POINT_NOT_EXIST    (-1)
+
+#define DISK_MOUNTED               (0)
+#define DISK_UNMOUNTED             (-1)
+
+#define DIR_EXIST                (0)
+#define DIR_NOT_EXIST            (-1)
+
+#define SCREEN_WIDTH             (720)
+#define SCREEN_HEIGHT            (480)
+
+static char *update_file_img = "/mnt/sd/KLAK_Update.img";
+static char *update_file 	 = "/tmp/KLAK_Update.zip";
+static char *update_path 	 = "/tmp/fw";
+
+static char *version_info_current = "/root/version.txt";
+static char *version_info_update  = "/tmp/fw/version.txt";
+
+static const char 	*bootlogo_file	= "/root/bootlogo.bmp";
+static char 		*execute_nvs	= "/root/execute_Tia.txt";
+static char 		*wlan0_file		= "/sys/class/net/wlan0";
+
+#pragma once
+class UpdateManager
+{
+public:
+	UpdateManager();
+    bool updateFW();
+	~UpdateManager();
+private:
+	bool exist_dir(char *dir);
+	bool exist_file(char*);
+	bool check_ver_file(char*, char*);
+};
+
+#endif //_UPDATEMANAGER_
